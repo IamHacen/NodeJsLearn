86cbe9b735ff0cb61065d20602a8ac3543e81fbb
IamHacen <43780893+IamHacen@users.noreply.github.com>
2024-04-04 13:45:21 +0500

    updated
    my app is updated now

---
+++ index.ts
@@ -0,0 +1,30 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+const answer= await inquirer.prompt([{message:"Enter Fisrt Number", type:"number", name: "firstNumber"},
+{message: "Enter second number", type:"number", name: "secondNumber"},
+{message: "Select one operator to perform action",
+type:"list",
+name: "operator",
+choices:["addition","subtraction", "multiplication", "division"]
+},
+]);
+
+//conditional statement
+
+if(answer.operator=== "addition"){
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if(answer.operator === "subtraction"){
+    console.log((answer.firstNumber) - (answer.secondNumber));
+}
+else if(answer.operator === "multiplication"){
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "division"){
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+
+else { 
+    console.log("please select a valid choice")
+}
+console.log(answer);
